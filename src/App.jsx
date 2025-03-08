bc9de60c397bf4277417dca62781ada56c70ceac
chandramadi <chandramadi6121@gmail.com>
2025-03-08 20:20:17 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from "./pages/Home"
 import SignUp from './Pages/Auth/SignUp';
+import Login from './Pages/Auth/Login';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/auth/signUp' element={<SignUp/>}/>
+      <Route path='/auth/signUp' element={<SignUp/>} />
+      <Route path='/auth/login' element={<Login/>} />
     </Routes>
   )
 }
