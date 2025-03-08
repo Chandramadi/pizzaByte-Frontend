a2f2069949e94baf0c024fdf969f461c28efe487
chandramadi <chandramadi6121@gmail.com>
2025-03-08 16:20:54 +0530

    Added Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div>
-      Home
+      <Home/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Icons/ArrowRightL.jsx
@@ -0,0 +1,17 @@
+
+export default function ArrowRightL() {
+    return (
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+        >
+        <path
+            fill="currentColor"
+            d="M24 12.16L18.24 6.4v4.24H0v3.04h18.24v4.24z"
+        ></path>
+        </svg>
+    )
+}
+  
