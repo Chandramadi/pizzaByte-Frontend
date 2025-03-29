1515b69595b4bd39fc53599a9ffe5669605dcd6f
chandramadi <chandramadi6121@gmail.com>
2025-03-29 16:34:48 +0530

    Update axiosInstance

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create(); // create a new instance of axios
 
 // Since you're using Vite, environment variables should be accessed via import.meta.env, not process.env.
-axiosInstance.defaults.baseURL = "https://pizzabyte-backend.onrender.com";
+axiosInstance.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 axiosInstance.defaults.withCredentials = true;
 // This allows Axios to send cookies along with requests when interacting with an API that sets cookies (such as authentication-related APIs).
